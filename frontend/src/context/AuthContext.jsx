11c9070b34cd15752913c028005843a6ec0ca2b9
Saurabh Bisht <saurabhbisht1042005@gmail.com>
2026-06-24 22:29:26 +0530

    Fix serverUrl string formatting in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "https://trendify-backends.onrender.com
+    let serverUrl = "https://trendify-backends.onrender.com"
 
     let value = {
        serverUrl
